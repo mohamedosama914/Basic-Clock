507795bd39466e0f74780d7b9e7586334a33de1f
Mohammed Osama <mohamedosama914@users.noreply.github.com>
2017-12-31 12:21:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function clock() 
+{
+   var digital = new Date();
+   var hours = digital.getHours();
+   var minutes = digital.getMinutes();
+   var seconds = digital.getSeconds();
+   if (minutes <= 9) minutes = "0" + minutes;
+   if (seconds <= 9) seconds = "0" + seconds;
+   dispTime = hours + ":" + minutes + ":" + seconds;
+
+   var basicclock = document.getElementById('basicclock');
+   basicclock.innerHTML = dispTime;
+   setTimeout("clock()", 1000);
+}
+clock();
